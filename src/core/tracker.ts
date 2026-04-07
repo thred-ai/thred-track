4226f7e774ef13792c942d59a3788033dcdb790b
Arta <128208448+arta-eth@users.noreply.github.com>
2026-04-07 15:09:00 -0400

    Refactor Vector script loading in Tracker class by removing the redundant check for multiple snippet inclusion, streamlining the code while maintaining error handling for loading issues.

--- src/core/tracker.ts
+++ src/core/tracker.ts
@@ -151,7 +151,7 @@ export class Tracker {
     script.type = 'text/javascript';
     script.id = '__vector__';
     script.textContent = [
-      '!function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);',
+      '!function(e,r){try{var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);',
       `window.vector.partnerId = "${fingerprint}";`,
       `vector.load("${token}");`,
     ].join('\n');

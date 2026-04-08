30639a16f53302b6d07a071a7a04fe0f54dfa1c5
Arta <128208448+arta-eth@users.noreply.github.com>
2026-04-08 12:15:13 -0400

    Refactor Vector snippet in Tracker class to remove window prefix from load function, streamlining script execution.

--- src/core/tracker.ts
+++ src/core/tracker.ts
@@ -153,7 +153,7 @@ export class Tracker {
     script.textContent = [
       '!function(e,r){try{var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);',
       `window.vector.partnerId = JSON.stringify({ userId: "${fingerprint}" });`,
-      `window.vector.load("${token}");`,
+      `vector.load("${token}");`,
     ].join('\n');
     document.head.appendChild(script);
     this.logger.log(`Vector token: ${token}`);
